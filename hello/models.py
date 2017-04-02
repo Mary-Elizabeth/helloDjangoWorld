4eb4d75d84349f661dbc3fc9b359899d07c26b31
Mary Dettenmaier <marydettenmaier@gmail.com>
2017-04-02 16:55:27 +0000

    add page view counter

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class PageCount(models.Model):
+    page = models.URLField(default='defaultPage')
+    visits = models.IntegerField(default=0)
+   
--- hello/views.py
+++ hello/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+import .models import PageCount
+
 # Create your views here.
 
 
 
 def index(request):
-    return HttpResponse("Hello, world at " + time.strftime("%c"))
+    row, create = PageCount.objects.get_or_create(page='index')
+    row.count += 1
+    row.save()
+    
+    return HttpResponse("Hello, visitor #" +str(row.count) + ". You visited this page @ " time.strftime("%c"))
